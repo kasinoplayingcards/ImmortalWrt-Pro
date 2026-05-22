99b7885e2ed6a574df8674f9380aa45dc9d200ee
kasinoplayingcards <126845560+kasinoplayingcards@users.noreply.github.com>
2026-05-22 10:27:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# 清理冲突
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/v2ray-core
 rm -rf feeds/packages/net/sing-box
 
-# IP 修改
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-
 # 关闭 IPv6
 sed -i 's/CONFIG_IPV6=y/CONFIG_IPV6=n/g' .config || true
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+
+echo "patched ok"
