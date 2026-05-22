a24e0b14d6c2e8729389da090737696f6f0976c5
kasinoplayingcards <126845560+kasinoplayingcards@users.noreply.github.com>
2026-05-22 22:28:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-echo "stable build start"
-
-# 清理冲突（只清必要的）
-rm -rf feeds/packages/net/sing-box
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/v2ray-core
-
-# 更新 feeds（关键）
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-echo "feeds ready"
